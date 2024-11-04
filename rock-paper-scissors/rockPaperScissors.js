c683681762fadea41e6da31f06011cfef82cc7cb
Chara <67203023+charadrim@users.noreply.github.com>
2024-11-04 20:32:56 +0100

    Add human choice function.

--- rock-paper-scissors/rockPaperScissors.js
+++ rock-paper-scissors/rockPaperScissors.js
@@ -1,19 +1,44 @@
+let rock = 'Rock';
+let paper = 'Paper';
+let scissors = 'Scissors';
+
+let computerScore;
+let humanScore;
+
 const getComputerChoice = () => {
   let computerChoice = Math.floor(Math.random() * 3);
 
   if (computerChoice === 0) {
-    let rock = 'Rock';
     console.log(rock);
     return rock;
   } else if (computerChoice === 1) {
-    let paper = 'Paper';
     console.log(paper);
     return paper;
   } else if (computerChoice === 2) {
-    let scissors = 'Scissors';
     console.log(scissors);
     return scissors;
   }
 };
 
 getComputerChoice();
+
+const getHumanChoice = () => {
+  let humanChoice = prompt('WHat is your choice? Rock, Paper or Scissors?');
+
+  if (humanChoice === rock) {
+    console.log(rock);
+    return rock;
+  } else if (humanChoice === paper) {
+    console.log(paper);
+    return paper;
+  } else if (humanChoice === scissors) {
+    console.log(scissors);
+    return scissors;
+  } else {
+    let errorMessage = 'Sorry, your choice is invalid.';
+    console.log(errorMessage);
+    return errorMessage;
+  }
+};
+
+getHumanChoice();
