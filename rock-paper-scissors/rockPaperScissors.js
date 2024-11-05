9ff9d114c5f56c647e5beaf4b59b616c20f289a7
Chara <67203023+charadrim@users.noreply.github.com>
2024-11-05 19:36:59 +0100

    Add playRound function

--- rock-paper-scissors/rockPaperScissors.js
+++ rock-paper-scissors/rockPaperScissors.js
@@ -1,37 +1,37 @@
-let rock = 'Rock';
-let paper = 'Paper';
-let scissors = 'Scissors';
+let rock = 'rock';
+let paper = 'paper';
+let scissors = 'scissors';
 
-let computerScore;
-let humanScore;
+let computerScore = 0;
+let humanScore = 0;
 
 const getComputerChoice = () => {
-  let computerChoice = Math.floor(Math.random() * 3);
+  let computerOption = Math.floor(Math.random() * 3);
 
-  if (computerChoice === 0) {
+  if (computerOption === 0) {
     console.log(rock);
     return rock;
-  } else if (computerChoice === 1) {
+  } else if (computerOption === 1) {
     console.log(paper);
     return paper;
-  } else if (computerChoice === 2) {
+  } else if (computerOption === 2) {
     console.log(scissors);
     return scissors;
   }
 };
 
-getComputerChoice();
-
 const getHumanChoice = () => {
-  let humanChoice = prompt('WHat is your choice? Rock, Paper or Scissors?');
+  let humanOption = prompt('What is your choice? Rock, Paper or Scissors?');
 
-  if (humanChoice === rock) {
+  let humanOptionLowerCase = humanOption.toLowerCase();
+
+  if (humanOptionLowerCase === rock) {
     console.log(rock);
     return rock;
-  } else if (humanChoice === paper) {
+  } else if (humanOptionLowerCase === paper) {
     console.log(paper);
     return paper;
-  } else if (humanChoice === scissors) {
+  } else if (humanOptionLowerCase === scissors) {
     console.log(scissors);
     return scissors;
   } else {
@@ -41,4 +41,36 @@ const getHumanChoice = () => {
   }
 };
 
-getHumanChoice();
+const playRound = (humanChoice, computerChoice) => {
+  let winningMessage = 'You win this round.';
+  let losingMessage = 'You lose this round.';
+  let tieMessage = "It's a tie.";
+
+  if (humanChoice === rock && computerChoice === scissors) {
+    console.log(winningMessage);
+    return winningMessage;
+  } else if (humanChoice === rock && computerChoice === paper) {
+    console.log(losingMessage);
+    return losingMessage;
+  } else if (humanChoice === paper && computerChoice === rock) {
+    console.log(winningMessage);
+    return winningMessage;
+  } else if (humanChoice === paper && computerChoice === scissors) {
+    console.log(losingMessage);
+    return losingMessage;
+  } else if (humanChoice === scissors && computerChoice === rock) {
+    console.log(losingMessage);
+    return losingMessage;
+  } else if (humanChoice === scissors && computerChoice === paper) {
+    console.log(winningMessage);
+    return winningMessage;
+  } else {
+    console.log(tieMessage);
+    return tieMessage;
+  }
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
